45561e9365cc723f4a8665fef775b50672506a4f
Hong-Mu <hongmuchae@gmail.com>
2023-02-26 02:46:33 +0900

    feat: 약관동의 체크박스 로직 작성

--- app/src/main/java/com/start/STart/ui/auth/register/PolicyActivity.kt
+++ app/src/main/java/com/start/STart/ui/auth/register/PolicyActivity.kt
@@ -2,6 +2,7 @@ package com.start.STart.ui.auth.register
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.CheckBox
 import androidx.appcompat.app.AppCompatActivity
 import com.start.STart.databinding.ActivityPolicyBinding
 
@@ -15,8 +16,36 @@ class PolicyActivity : AppCompatActivity() {
     }
 
     private fun initViewListeners() {
+        updateCheckAllCheckBox()
         binding.btnNext.setOnClickListener {
             startActivity(Intent(this, ValidateStudentInfoActivity::class.java))
         }
+        binding.checkAll.setOnCheckedChangeListener { _, isChecked ->
+            binding.btnNext.isEnabled = isChecked
+        }
+    }
+
+    private fun updateCheckAllCheckBox() {
+        val checkBoxList = listOf(binding.checkPrivacyPolicy, binding.checkService, binding.checkLocation)
+
+        binding.checkAll.setOnClickListener {
+            checkAllCheckBoxes(checkBoxList, binding.checkAll.isChecked)
+        }
+
+        checkBoxList.forEach {
+            it.setOnCheckedChangeListener { _, _ ->
+                if (!it.isChecked) {
+                    binding.checkAll.isChecked = false
+                } else {
+                    val allChecked = checkBoxList.all { it.isChecked }
+                    binding.checkAll.isChecked = allChecked
+                }
+            }
+        }
+    }
+
+    private fun checkAllCheckBoxes(checkBoxList: List<CheckBox>, isChecked: Boolean) {
+        binding.checkAll.isChecked = isChecked
+        checkBoxList.forEach { it.isChecked = isChecked }
     }
 }
