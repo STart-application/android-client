bc4421be0d51f6dd64c172b9e641dc157682b6b6
Hong-Mu <hongmuchae@gmail.com>
2023-03-01 03:43:21 +0900

    feat: 학생증 이미지 업로드 추가

--- app/src/main/java/com/start/STart/ui/auth/register/StudentCardUploadActivity.kt
+++ app/src/main/java/com/start/STart/ui/auth/register/StudentCardUploadActivity.kt
@@ -2,11 +2,25 @@ package com.start.STart.ui.auth.register
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.start.STart.databinding.ActivityValidateStudentCardBinding
 
 class StudentCardUploadActivity : AppCompatActivity() {
     private val binding by lazy { ActivityValidateStudentCardBinding.inflate(layoutInflater) }
+
+    private val imageLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+        uri?.let {
+            Glide.with(this)
+                .load(it)
+                .centerCrop()
+                .into(binding.inputStudentCard)
+
+            binding.btnNext.isEnabled = true
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -14,6 +28,9 @@ class StudentCardUploadActivity : AppCompatActivity() {
     }
 
     private fun initViewListeners() {
+        binding.layoutInputStudentCard.setOnClickListener {
+            imageLauncher.launch("image/*")
+        }
         binding.btnNext.setOnClickListener {
             startActivity(Intent(this, RegisterCompleteActivity::class.java))
         }
