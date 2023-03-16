3d681295b47003f87ac526b3ab35b918176054de
Hong-Mu <hongmuchae@gmail.com>
2023-03-16 13:22:30 +0900

    feat: 토큰 로직 보완

--- app/src/main/java/com/start/STart/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/start/STart/ui/splash/SplashActivity.kt
@@ -5,9 +5,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.start.STart.api.ApiClient
-import com.start.STart.api.TokenInterceptor
 import com.start.STart.databinding.ActivitySplashBinding
-import com.start.STart.ui.auth.login.LoginActivity
 import com.start.STart.ui.auth.login.LoginOrSkipActivity
 import com.start.STart.ui.home.HomeActivity
 import com.start.STart.util.TokenHelper
@@ -19,18 +17,25 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        
+        // 로그인 시도
+        tryLogin()
 
-        lifecycleScope.launch(Dispatchers.IO) {
-            val isSuccessful = TokenHelper.tryLoginWithAccessToken()
-            if(isSuccessful) {
-                startActivity(Intent(applicationContext, HomeActivity::class.java))
-            } else {
-                startActivity(Intent(applicationContext, LoginOrSkipActivity::class.java))
-            }
-        }
-
+        // TODO: 로그인 로직 정상 동작 확인 후 버튼 삭제
         binding.btnMove.setOnClickListener {
             startActivity(Intent(this, LoginOrSkipActivity::class.java))
+            finish()
+        }
+    }
+    
+    private fun tryLogin() = lifecycleScope.launch(Dispatchers.IO) {
+        val isSuccessful = TokenHelper.tryLoginWithAccessToken()
+        if(isSuccessful) {
+            startActivity(Intent(applicationContext, HomeActivity::class.java))
+            finish()
+        } else {
+            ApiClient.disableToken()
+            startActivity(Intent(applicationContext, LoginOrSkipActivity::class.java))
             finish()
         }
     }
