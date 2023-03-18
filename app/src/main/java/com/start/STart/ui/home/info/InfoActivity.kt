c4fbf10172eff079110af6dd3bf5188b71c7237e
100SongH <thdgml322@seoultech.ac.kr>
2023-03-18 18:03:42 +0900

    style: Information 화면 레이아웃

--- app/src/main/java/com/start/STart/ui/home/info/InfoActivity.kt
+++ app/src/main/java/com/start/STart/ui/home/info/InfoActivity.kt
@@ -1,7 +1,9 @@
 package com.start.STart.ui.home.info
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import com.start.STart.R
 import com.start.STart.databinding.ActivityInfoBinding
 
@@ -13,6 +15,7 @@ class InfoActivity : AppCompatActivity() {
         Info2Fragment(),
         Info3Fragment(),
     )
+    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -25,22 +28,78 @@ class InfoActivity : AppCompatActivity() {
             .replace(R.id.container, tabList[0])
             .commit()
 
-        binding.btn1.setOnClickListener {
+
+        binding.txt1.setOnClickListener {
+
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, tabList[0])
                 .commit()
+
+            binding.txt1.setTextColor(ContextCompat.getColor(this, R.color.dream_purple))
+            binding.txt2.setTextColor(ContextCompat.getColor(this, R.color.dream_purple_ghost))
+            binding.txt3.setTextColor(ContextCompat.getColor(this, R.color.dream_purple_ghost))
+
+            binding.btn1.layoutParams.height = (resources.displayMetrics.density * 5).toInt()
+            binding.btn1.setBackgroundColor(ContextCompat.getColor(this, R.color.dream_purple))
+
+            binding.btn2.layoutParams.height = (resources.displayMetrics.density * 3).toInt()
+            binding.btn2.setBackgroundColor(ContextCompat.getColor(this, R.color.dream_purple_ghost))
+
+            binding.btn3.layoutParams.height = (resources.displayMetrics.density * 3).toInt()
+            binding.btn3.setBackgroundColor(ContextCompat.getColor(this, R.color.dream_purple_ghost))
+
+
+
         }
 
-        binding.btn2.setOnClickListener {
+        binding.txt2.setOnClickListener {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, tabList[1])
                 .commit()
+
+            binding.txt1.setTextColor(ContextCompat.getColor(this, R.color.dream_purple_ghost))
+            binding.txt2.setTextColor(ContextCompat.getColor(this, R.color.dream_purple))
+            binding.txt3.setTextColor(ContextCompat.getColor(this, R.color.dream_purple_ghost))
+
+            binding.btn1.layoutParams.height = (resources.displayMetrics.density * 3).toInt()
+            binding.btn1.setBackgroundColor(ContextCompat.getColor(this, R.color.dream_purple_ghost))
+
+            binding.btn2.layoutParams.height = (resources.displayMetrics.density * 5).toInt()
+            binding.btn2.setBackgroundColor(ContextCompat.getColor(this, R.color.dream_purple))
+
+            binding.btn3.layoutParams.height = (resources.displayMetrics.density * 3).toInt()
+            binding.btn3.setBackgroundColor(ContextCompat.getColor(this, R.color.dream_purple_ghost))
+
+            binding.btn1.requestLayout()
+            binding.btn2.requestLayout()
+            binding.btn3.requestLayout()
+
+
         }
 
-        binding.btn3.setOnClickListener {
+        binding.txt3.setOnClickListener {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, tabList[2])
                 .commit()
+
+            binding.txt1.setTextColor(ContextCompat.getColor(this, R.color.dream_purple_ghost))
+            binding.txt2.setTextColor(ContextCompat.getColor(this, R.color.dream_purple_ghost))
+            binding.txt3.setTextColor(ContextCompat.getColor(this, R.color.dream_purple))
+
+            binding.btn1.layoutParams.height = (resources.displayMetrics.density * 3).toInt()
+            binding.btn1.setBackgroundColor(ContextCompat.getColor(this, R.color.dream_purple_ghost))
+
+            binding.btn2.layoutParams.height = (resources.displayMetrics.density * 3).toInt()
+            binding.btn2.setBackgroundColor(ContextCompat.getColor(this, R.color.dream_purple_ghost))
+
+            binding.btn3.layoutParams.height = (resources.displayMetrics.density * 5).toInt()
+            binding.btn3.setBackgroundColor(ContextCompat.getColor(this, R.color.dream_purple))
+
+            binding.btn1.requestLayout()
+            binding.btn2.requestLayout()
+            binding.btn3.requestLayout()
+
+
         }
     }
 }
