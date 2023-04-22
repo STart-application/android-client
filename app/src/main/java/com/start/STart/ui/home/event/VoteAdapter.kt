e659f0d8ffad19f0acb29a42f4598fff8603e561
100SongH <thdgml322@seoultech.ac.kr>
2023-04-22 14:32:24 +0900

    feat: 방탈출, 투표 일부

---
+++ app/src/main/java/com/start/STart/ui/home/event/VoteAdapter.kt
@@ -0,0 +1,67 @@
+package com.start.STart.ui.home.event
+
+import android.content.Intent
+import android.content.res.ColorStateList
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.start.STart.R
+import com.start.STart.api.banner.Vote
+import com.start.STart.databinding.ItemVoteBinding
+import com.start.STart.ui.home.event.VoteAdapter.VoteViewHolder
+
+class VoteAdapter : RecyclerView.Adapter<VoteAdapter.VoteViewHolder>() {
+
+    var list: List<Vote> = mutableListOf()
+
+        set(value) {
+            field = value
+        }
+
+    inner class VoteViewHolder(var binding: ItemVoteBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(vote: Vote){
+            binding.title.text = vote.title
+
+            binding.root.setOnClickListener {
+                val context = binding.root.context
+                context.startActivity(Intent(context, DetailVoteActivity::class.java))
+            }
+
+            /*
+            val purple_ghost = ContextCompat.getColor(binding.status.context, R.color.dream_purple_ghost)
+            val purple = ContextCompat.getColor(binding.status.context, R.color.dream_purple)
+            val gray = ContextCompat.getColor(binding.status.context, R.color.dream_gray)
+            val green = ContextCompat.getColor(binding.status.context, R.color.dream_green)
+
+            when(vote.status) {
+                "PROCEEDING" -> {
+                    binding.status.backgroundTintList = ColorStateList.valueOf(green)
+                    binding.status.text = "투표 가능"
+                }
+                "END" -> {
+                    binding.status.backgroundTintList = ColorStateList.valueOf(gray)
+                    binding.status.text = "투표 마감"
+                }
+                "BEFORE" -> {
+                    binding.status.backgroundTintList = ColorStateList.valueOf(purple_ghost)
+                    binding.status.text = "투표 예정"
+                }
+            }
+
+             */
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VoteViewHolder {
+        val binding = ItemVoteBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return VoteViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: VoteViewHolder, position: Int) {
+        holder.bind(list[position])
+    }
+
+    override fun getItemCount() = list.size
+
+}
---
+++ app/src/main/java/com/start/STart/ui/home/event/VoteListAdapter.kt
@@ -0,0 +1,37 @@
+package com.start.STart.ui.home.event
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.start.STart.api.banner.Vote
+import com.start.STart.databinding.ItemListBinding
+import com.start.STart.ui.home.event.VoteListAdapter.VoteListViewHolder
+
+class VoteListAdapter : RecyclerView.Adapter<VoteListViewHolder>() {
+    var list: List<Vote> = mutableListOf()
+
+        set(value) {
+            field = value
+        }
+
+    inner class VoteListViewHolder(var binding: ItemListBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(vote: Vote){
+
+
+        }
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VoteListViewHolder {
+        val binding = ItemListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return VoteListViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: VoteListViewHolder, position: Int) {
+        holder.bind(list[position])
+    }
+
+    override fun getItemCount() = list.size
+
+}
+
+
+
