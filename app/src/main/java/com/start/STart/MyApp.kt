22c5465ad8eb923704b086aeb2436b08c410249b
Hong-Mu <hongmuchae@gmail.com>
2023-04-24 13:00:27 +0900

    [공통] 다크모드 방지 코드 추가

--- app/src/main/java/com/start/STart/MyApp.kt
+++ app/src/main/java/com/start/STart/MyApp.kt
@@ -3,6 +3,7 @@ package com.start.STart
 import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
+import androidx.appcompat.app.AppCompatDelegate
 import com.jakewharton.threetenabp.AndroidThreeTen
 import com.start.STart.util.PreferenceManager
 
@@ -18,7 +19,12 @@ class MyApp: Application() {
 
     override fun onCreate() {
         super.onCreate()
+        // 다크모드 비활성화
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        // 시간 관련 유틸
         AndroidThreeTen.init(this)
+
         // TODO: Hilt 라이브러리를 이용한 의존성 주입 (시간남으면 공부하고 적용)
         PreferenceManager.init(applicationContext)
         context = applicationContext
