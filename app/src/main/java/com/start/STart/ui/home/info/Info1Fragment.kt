e1c7236c7063a667a91facac97a708359bfbcaf3
Hong-Mu <hongmuchae@gmail.com>
2023-04-24 19:58:46 +0900

    [상시사업] 설명 수정

--- app/src/main/java/com/start/STart/ui/home/info/Info1Fragment.kt
+++ app/src/main/java/com/start/STart/ui/home/info/Info1Fragment.kt
@@ -1,15 +1,13 @@
 package com.start.STart.ui.home.info
 
-import android.graphics.drawable.PictureDrawable
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.start.STart.R
-import com.start.STart.databinding.FragmentInfo1Binding
 
 class Info1Fragment : Fragment() {
 
